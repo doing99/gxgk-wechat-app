fcabd0ed6bc741a8e07c488a923ebd443a1f0ff3
白月秋见心 <ourweijiang@gmail.com>
2016-12-26 23:02:03 +0800

    完成绑定饭卡

--- pages/more/append.js
+++ pages/more/append.js
@@ -69,8 +69,7 @@ Page({
         bind_type: _this.data.bind_type
       },
       success: function (res) {
-        if (res.data && res.statusCode === 200) {
-          console.log(res)
+        if (res.statusCode === 200 && res.data.errmsg == 'ok') {
           app.showLoadToast('请稍候');
           //清除缓存
           if (app.cache) {
@@ -84,6 +83,15 @@ Page({
               duration: 1500
             });
             if (!app.user.is_teacher) {
+              if (!app.user.is_bind_mealcard) {
+                jump_url = 'append?type=mealcard';
+              } else if (!app.user.is_bind_library){
+                jump_url = 'append?type=library';
+              }
+              else {
+                wx.navigateBack();
+                return;
+              }
               setTimeout(function () {
                 wx.showModal({
                   title: '提示',
@@ -93,7 +101,7 @@ Page({
                   success: function (res) {
                     if (res.confirm) {
                       wx.redirectTo({
-                        url: 'append'
+                        url: jump_url
                       });
                     } else {
                       wx.navigateBack();
@@ -107,7 +115,7 @@ Page({
           });
         } else {
           wx.hideToast();
-          app.showErrorModal(res.data.message, '绑定失败');
+          app.showErrorModal(res.data.errmsg, '绑定失败');
         }
       },
       fail: function (res) {

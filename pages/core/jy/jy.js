60f20e48e516fb8fc9800d45327a16e3918f3e49
白月秋见心 <ourweijiang@gmail.com>
2017-01-16 00:30:14 +0800

    完成借书查询

--- pages/core/jy/jy.js
+++ pages/core/jy/jy.js
@@ -21,21 +21,22 @@ Page({
   },
   getData: function () {
     var _this = this;
-    if (!app.user.we.info.id || !app._user.we.info.name) {
+    if (!app.user.wxinfo.id || !app.user.is_bind_library) {
       _this.setData({
         remind: '未绑定'
       });
       return false;
     }
     wx.request({
-      url: app._server + "/api/get_booklist.php",
+      url: app.server + "/api/users/get_user_library",
       method: 'POST',
-      data: { session_id: app.user.wxinfo.id },
+      data: {
+         session_id: app.user.wxinfo.id
+          },
       success: function (res) {
         if (res.data && res.statusCode == 200) {
-          var info = res.data.data;
-          info.nothing = !parseInt(info.books_num) && (!info.book_list || !info.book_list.length);
-          _this.setData({
+          var info = res.data.msg;
+         _this.setData({
             jyData: info,
             remind: ''
           });

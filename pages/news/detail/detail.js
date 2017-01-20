a11395d7465014fcc809ca28f747a638a1b26c97
白月秋见心 <ourweijiang@gmail.com>
2017-01-20 16:07:55 +0800

    增加下载功能，但是微信只能下载规定格式的，所以备用

--- pages/news/detail/detail.js
+++ pages/news/detail/detail.js
@@ -27,27 +27,27 @@ module.exports.ipage = {
     return {
       title: _this.data.title,
       desc: '莞香小喵 - 资讯详情',
-      path: 'pages/news/'+_this.data.type+'/'+_this.data.type+'_detail?type='+_this.data.type+'&id='+_this.data.id
+      path: 'pages/news/' + _this.data.type + '/' + _this.data.type + '_detail?type=' + _this.data.type + '&id=' + _this.data.id
     }
   },
 
-  convertHtmlToText: function(inputText){
+  convertHtmlToText: function (inputText) {
     var returnText = "" + inputText;
     returnText = returnText.replace(/<\/?[^>]*>/g, '').replace(/[ | ]*\n/g, '\n').replace(/ /ig, '')
-                  .replace(/&mdash/gi,'-').replace(/&ldquo/gi,'“').replace(/&rdquo/gi,'”');
+      .replace(/&mdash/gi, '-').replace(/&ldquo/gi, '“').replace(/&rdquo/gi, '”');
     return returnText;
   },
-  
-  onLoad: function(options){
+
+  onLoad: function (options) {
     var _this = this;
-    app.loginLoad(function(){
+    app.loginLoad(function () {
       _this.loginHandler.call(_this, options);
     });
   },
-  loginHandler: function(options){
+  loginHandler: function (options) {
     var _this = this;
-    
-    if(!options.type || !options.id) {
+
+    if (!options.type || !options.id) {
       _this.setData({
         remind: '404'
       });
@@ -61,8 +61,8 @@ module.exports.ipage = {
     wx.request({
       url: app.server + '/api/get_news_detail',
       data: options,
-      success: function(res){
-        if(res.data.data && res.statusCode == 200){
+      success: function (res) {
+        if (res.data.data && res.statusCode == 200) {
           var info = res.data.data;
           _this.setData({
             date: info.time || "",  // 发布日期
@@ -76,8 +76,8 @@ module.exports.ipage = {
           });
 
           // 如果存在附件则提取附件里面的信息
-          if(info.fjlist && info.fjlist.length){
-            info.fjlist.map(function(e){
+          if (info.fjlist && info.fjlist.length) {
+            info.fjlist.map(function (e) {
               //判断是否支持预览
               e.preview = (e.fjtitle.search(/\.doc|.xls|.ppt|.pdf|.docx|.xlsx|.pptx$/) !== -1);
               return e;
@@ -87,14 +87,14 @@ module.exports.ipage = {
               files_list: info.fjlist
             });
           }
-        }else{
+        } else {
           app.showErrorModal(res.data.error);
           _this.setData({
             remind: res.data.error || '未知错误'
           });
         }
       },
-      fail: function(){
+      fail: function () {
         app.showErrorModal(res.errMsg);
         _this.setData({
           remind: '网络错误'
@@ -103,9 +103,9 @@ module.exports.ipage = {
     })
   },
 
-  getFj: function(e){
+  getFj: function (e) {
     var _this = this;
-    if(!e.currentTarget.dataset.preview){
+    if (!e.currentTarget.dataset.preview) {
       app.showErrorModal('不支持该格式文件预览！', '无法预览');
       return;
     }
@@ -113,7 +113,7 @@ module.exports.ipage = {
       title: '提示',
       content: '预览或下载附件需要消耗流量，是否继续？',
       confirmText: '继续',
-      success: function(res) {
+      success: function (res) {
         if (res.confirm) {
           app.showLoadToast('下载中，请稍候');
           wx.showNavigationBarLoading();
@@ -123,7 +123,7 @@ module.exports.ipage = {
           //下载
           wx.downloadFile({
             url: e.currentTarget.dataset.url,
-            success: function(res) {
+            success: function (res) {
               var filePath = res.tempFilePath;
               //预览
               wx.openDocument({
@@ -134,7 +134,7 @@ module.exports.ipage = {
                 fail: function (res) {
                   app.showErrorModal(res.errMsg, '预览失败');
                 },
-                complete: function(){
+                complete: function () {
                   wx.hideNavigationBarLoading();
                   wx.hideToast();
                   _this.setData({
@@ -143,8 +143,76 @@ module.exports.ipage = {
                 }
               });
             },
-            fail: function(res){
+            fail: function (res) {
               app.showErrorModal(res.errMsg, '下载失败');
+              wx.hideNavigationBarLoading();
+              wx.hideToast();
+              _this.setData({
+                file_loading: false
+              });
+            }
+          });
+        }
+      }
+    });
+  },
+  downFile: function (e) {
+    var _this = this;
+    wx.showModal({
+      title: '提示',
+      content: '下载附件需要消耗流量，是否继续？',
+      confirmText: '继续',
+      success: function (res) {
+        if (res.confirm) {
+          app.showLoadToast('下载中，请稍候', 10000);
+          wx.showNavigationBarLoading();
+          _this.setData({
+            file_loading: true
+          });
+          //清除数据
+          wx.getSavedFileList({
+            success: function (res) {
+              if (res.fileList.length > 0) {
+                wx.removeSavedFile({
+                  filePath: res.fileList[0].filePath,
+                  complete: function (res) {
+                    console.log(res)
+                  }
+                })
+              }
+              console.log(res.fileList)
+            }
+          })
+          //下载
+          wx.downloadFile({
+            url: e.currentTarget.dataset.url,
+            //url: "https://sr.lastfighting.com/e/upload/PSTools.zip",
+            success: function (res) {
+              var filePath = res.tempFilePath;
+              if (filePath) {
+                wx.saveFile({
+                  tempFilePath: filePath,
+                  success: function (res) {
+                    var savedFilePath = res.savedFilePath;
+                    //保存成功
+                    wx.getSavedFileList({
+                      success: function (res) {
+                        console.log(res.fileList)
+                      }
+                    })
+                  },
+                  fail: function (res) {
+                    app.showErrorModal(res.errMsg, '保存失败');
+                  },
+                })
+              } else {
+                app.showErrorModal("下载过程出错，错误代码：" + res.statusCode, '下载失败');
+              }
+            },
+            fail: function (res) {
+              app.showErrorModal(res.errMsg, '下载失败');
+            },
+            complete: function () {
               wx.hideNavigationBarLoading();
               wx.hideToast();
               _this.setData({
